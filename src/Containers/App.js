c000ef9236cea6c7c38af5c0e37eafabd2a6754c
skylerstyron <skylerstyrondev@gmail.com>
2022-08-08 17:52:32 -0500

    initial commit

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -42,6 +42,7 @@ class App extends Component {
                         <CardList robots={filteredRobots} />
                     </ErrorBoundry>
                     </Scroll>
+                    
                 </div>
             );
     }
